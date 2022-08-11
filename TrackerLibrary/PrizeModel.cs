9225fd67d6629df89dce77077f6d96581a3c24b4
Azeem <Azeem@HL-DEV-INT21>
2022-08-11 13:14:53 +0100

    Add project files.

---
+++ TrackerLibrary/PrizeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibrary
+{
+    class PrizeModel
+    {
+        public int PlaceNumber { get; set; }
+        public string PlaceName { get; set; }
+        public decimal PrizeAmount  { get; set; }
+        public double Percentage { get; set; }
+    }
+}
